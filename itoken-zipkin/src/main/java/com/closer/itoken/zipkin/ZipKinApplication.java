a2a79a689fb131e599c8b7a1b32844d61f336f69
CloserWU <18969946575@163.com>
2019-05-10 16:01:28 +0800

    itoken

--- itoken-zipkin/src/main/java/com/closer/itoken/zipkin/ZipKinApplication.java
+++ itoken-zipkin/src/main/java/com/closer/itoken/zipkin/ZipKinApplication.java
@@ -2,11 +2,13 @@ package com.closer.itoken.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZipkinServer
 public class ZipKinApplication {
     public static void main(String[] args) {
